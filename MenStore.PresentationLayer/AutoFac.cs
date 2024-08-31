21b33c6e55114ac7376ed2f9bfdeb6cf3f3afc31
kareemelsonbaty <71130316+Kareem7777@users.noreply.github.com>
2024-08-31 17:50:37 +0300

    Create Forms for admin & one product

---
+++ MenStore.PresentationLayer/AutoFac.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using AutoMapper;
+using MenStore.Application.Contracts;
+using MenStore.Application.Mapper;
+using MenStore.Application.Services;
+using MenStore.Application.Services.ProductServices;
+using MenStore.Context;
+using MenStore.Infrastructure;
+
+namespace MenStore.PresentationLayer
+{
+    public class AutoFac
+    {
+        public static Autofac.IContainer Inject()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<ProductService>().As<IProductService>();
+            builder.RegisterType<ProductRepository>().As<IProductRepository>();
+            builder.RegisterType<StoreContext>().As<StoreContext>();
+            builder.Register(c => new MapperConfiguration(a =>
+            {
+                a.AddProfile<AutoMapperProfile>();
+
+            }));
+
+            builder.Register(c => c.Resolve<MapperConfiguration>().CreateMapper(c.Resolve))
+                .As<IMapper>()
+                .InstancePerLifetimeScope();
+            return builder.Build();
+        }
+    }
+}
---
+++ MenStore.PresentationLayer/ProductForAdmin.Designer.cs
@@ -0,0 +1,157 @@
+﻿using System.Windows.Forms;
+
+namespace MenStore.PresentationLayer
+{
+    partial class ProductForAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            btnNext = new Button();
+            btnPrevious = new Button();
+            menuStrip1 = new MenuStrip();
+            loadToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            toolStripComboBox1 = new ToolStripComboBox();
+            searchToolStripMenuItem = new ToolStripMenuItem();
+            toolStripTextBox1 = new ToolStripTextBox();
+            btnHome = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(-1, 35);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(791, 302);
+            dataGridView1.TabIndex = 0;
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(511, 379);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(113, 43);
+            btnNext.TabIndex = 1;
+            btnNext.Text = "next";
+            btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            btnPrevious.Location = new Point(162, 379);
+            btnPrevious.Name = "btnPrevious";
+            btnPrevious.Size = new Size(94, 43);
+            btnPrevious.TabIndex = 2;
+            btnPrevious.Text = "previous";
+            btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { loadToolStripMenuItem, saveToolStripMenuItem, toolStripComboBox1, searchToolStripMenuItem, toolStripTextBox1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(790, 32);
+            menuStrip1.TabIndex = 5;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // loadToolStripMenuItem
+            // 
+            loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            loadToolStripMenuItem.Size = new Size(56, 28);
+            loadToolStripMenuItem.Text = "Load";
+            loadToolStripMenuItem.Click += loadToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(54, 28);
+            saveToolStripMenuItem.Text = "Save";
+            // 
+            // toolStripComboBox1
+            // 
+            toolStripComboBox1.Name = "toolStripComboBox1";
+            toolStripComboBox1.Size = new Size(121, 28);
+            toolStripComboBox1.Click += toolStripComboBox1_Click;
+            // 
+            // searchToolStripMenuItem
+            // 
+            searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            searchToolStripMenuItem.Size = new Size(67, 28);
+            searchToolStripMenuItem.Text = "Search";
+            // 
+            // toolStripTextBox1
+            // 
+            toolStripTextBox1.Name = "toolStripTextBox1";
+            toolStripTextBox1.Size = new Size(100, 28);
+            // 
+            // btnHome
+            // 
+            btnHome.Location = new Point(339, 379);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(94, 43);
+            btnHome.TabIndex = 6;
+            btnHome.Text = "Home";
+            btnHome.UseVisualStyleBackColor = true;
+            // 
+            // ProductForAdmin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(790, 450);
+            Controls.Add(btnHome);
+            Controls.Add(btnPrevious);
+            Controls.Add(btnNext);
+            Controls.Add(dataGridView1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "ProductForAdmin";
+            Text = "productForAdmin";
+            Load += productForAdmin_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnNext;
+        private Button btnPrevious;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripComboBox toolStripComboBox1;
+        private ToolStripMenuItem searchToolStripMenuItem;
+        private ToolStripTextBox toolStripTextBox1;
+        private Button btnHome;
+    }
+}
